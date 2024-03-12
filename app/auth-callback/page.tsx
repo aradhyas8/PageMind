91e49c12bc68bedda9ebc2aaf08e8c66ac33ccdd
Aradhya <aradhya@my.yorku.ca>
2024-03-12 00:37:42 -0400

    TRPC

---
+++ app/auth-callback/page.tsx
@@ -0,0 +1,10 @@
+import { useSearchParams, useRouter } from 'next/navigation'
+import React from 'react'
+
+const page = () => {
+    const router = useRouter()
+    const searchParams = useSearchParams()
+    const origin = searchParams.get('origin')
+}
+
+export default page
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,17 @@
+import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { redirect } from 'next/navigation'
+import React from 'react'
+
+async function page() {
+  
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+    if(!user || !user.id) redirect('/auth-callback?origin=dashboard')
+
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
