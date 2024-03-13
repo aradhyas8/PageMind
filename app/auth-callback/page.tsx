18b1dc2f9ba24c3938b47a11e3d4b9c477dd558d
Aradhya <aradhya@my.yorku.ca>
2024-03-13 19:45:36 -0400

    Prisma db index

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -3,7 +3,7 @@
 import { useRouter, useSearchParams } from 'next/navigation';
 import { trpc } from '../_trpc/client';
 
-const Page = () => {
+const Page = async () => {
     const router = useRouter()
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
