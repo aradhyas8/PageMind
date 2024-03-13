d39cd72ae308abe6f486b5fd4925f51e585527df
Aradhya <aradhya@my.yorku.ca>
2024-03-13 18:33:08 -0400

    Prisma

--- app/auth-callback/page.tsx
+++ app/auth-callback/page.tsx
@@ -1,15 +1,19 @@
-/* use client */
 'use client'
+
 import { useRouter, useSearchParams } from 'next/navigation';
-import React from 'react';
 import { trpc } from '../_trpc/client';
 
 const Page = () => {
-    const router = useRouter();
+    const router = useRouter()
     const searchParams = useSearchParams();
     const origin = searchParams.get('origin');
 
-    const { data, isLoading } = ();
+    const { data, isLoading } = trpc.authCallback.useQuery(undefined, {onSuccess: ({success}) => {
+        if(success){
+            //user is synced to db.
+            router.push(origin ? `/${origin}` : '/dashboard')
+        }
+    }});
 
     // Make sure to use effect hooks or other logic here
 
