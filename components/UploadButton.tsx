4249ff00915f734eac1cc139bae90fdf60c737e8
Aradhya <aradhya@my.yorku.ca>
2024-03-17 14:23:46 -0400

    UPLOAD bUTTON

--- components/UploadButton.tsx
+++ components/UploadButton.tsx
@@ -5,10 +5,37 @@ import { DialogTrigger } from '@radix-ui/react-dialog';
 import { Button } from './ui/button';
 import Dropzone from 'react-dropzone'
 import { Cloud, File } from 'lucide-react';
+import { Progress } from './ui/progress';
+import { set } from 'date-fns';
 
 const UploadDropZone = () => {
+    const [isUploading, setIsUploading] = React.useState<boolean>(true)
+    const [uploadProgress, setUploadProgress] = React.useState<number>(0) 
+
+    const startSimulatorProgress = () => {
+        setUploadProgress(0)
+
+        const interval = setInterval(() => {
+            setUploadProgress((prev) => {
+                if(prev >=95) {
+                    clearInterval(interval)
+                    return prev
+                }
+                return prev + 5
+            })
+        },500)
+        return interval
+    }
+
     return <Dropzone multiple={false} onDrop={(acceptedFile) => {
-        console.log(acceptedFile)
+        setIsUploading(true)
+
+        const ProgressInterval = startSimulatorProgress()
+        
+        //handle file uploading
+
+        clearInterval(ProgressInterval)
+        setUploadProgress(100)
     }}>
         {({getRootProps, getInputProps, acceptedFiles})=> (
             <div {...getRootProps()} className='border h-64 m-4 border-dashed border-gray-300 rounded-lg'>
@@ -23,13 +50,18 @@ const UploadDropZone = () => {
                             </p>
                             <p className='text-xs text-zinc-500'>PDF(up to 4MB)</p>
                         </div>
-                        
+
                         {acceptedFiles && acceptedFiles[0] ? (
                             <div className='max-w-xs bg-white flex items-center rounded-md overflow-hidden outline outline-[1px] outline-zinc-200 divide-x divide-zinc-200'>
                                 <div className='px-3 py-2 h-full grid place-items-center'>
                                     <File className='h-4 w-4 text-blue-500'/>
                                 </div>
+                                <div className='px-3 py-2 h-full text-sm truncate'>{acceptedFiles[0].name}</div>
                             </div>
+                        ) : null}
+
+                        {isUploading ? (
+                            <div className='w-full mt-4 max-w-xs mx-auto'><Progress value={uploadProgress} className='h-1 w-full bg-zinc-200 rounded'/></div>
                         ) : null}
                     </label>
                 </div>
