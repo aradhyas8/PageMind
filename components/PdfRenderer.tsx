d911bcc1665b6f5f3378f85db0b523a785f0bcd0
Aradhya <aradhya@my.yorku.ca>
2024-03-19 19:44:31 -0400

    PDF

--- components/PdfRenderer.tsx
+++ components/PdfRenderer.tsx
@@ -1,3 +1,6 @@
+import {Document, Page} from 'react-pdf'
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 
 function PdfRenderer() {
@@ -5,6 +8,13 @@ function PdfRenderer() {
     <div className="w-full bg-white rounded-md flex flex-col items-center">
         <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
             <div className="flex items-center gap-1.5">Top Bar</div>
+        </div>
+        <div className="flex-1 w-full max-h-screen">
+          <div>
+            <Document className='max-h-full'>
+              <Page pageNumber={1}/>
+            </Document>
+          </div>
         </div>
     </div>
   )
