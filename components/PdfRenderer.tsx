4a6f04e93690cf855f9deb15d8a8b73874aebd36
Aradhya <aradhya@my.yorku.ca>
2024-03-20 20:34:01 -0400

    FileRenderer

--- components/PdfRenderer.tsx
+++ components/PdfRenderer.tsx
@@ -1,26 +1,31 @@
-import {Document, Page} from 'react-pdf'
+'use client'
+
+import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`
+
 interface PdfRendererProps {
-  url: string
+  url: string;
 }
 
-function PdfRenderer = ({url}: PdfRendererProps) => {
+const PdfRenderer = ({ url }: PdfRendererProps) => {
   return (
     <div className="w-full bg-white rounded-md flex flex-col items-center">
-        <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
-            <div className="flex items-center gap-1.5">Top Bar</div>
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        <div className="flex items-center gap-1.5">Top Bar</div>
+      </div>
+      <div className="flex-1 w-full max-h-screen">
+        <div>
+          <Document file={url} className='max-h-full'>
+            <Page pageNumber={1} />
+          </Document>
         </div>
-        <div className="flex-1 w-full max-h-screen">
-          <div>
-            <Document file={} className='max-h-full'>
-              <Page pageNumber={1}/>
-            </Document>
-          </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default PdfRenderer
+export default PdfRenderer;
